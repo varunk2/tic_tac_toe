565bf0bf10f033fbdff4559cfb315e65a9b18129
Varun Sharma <varunk22110@gmail.com>
2021-02-06 18:49:41 +0530

    Created basic setup. Implemented GameController and Button controllers.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+
+    private string _playerSide;
+
+    private void Awake() {
+        SetGameControllerReferenceOnButtons();
+        _playerSide = "X";
+    }
+
+    private void SetGameControllerReferenceOnButtons() {
+        for (int i = 0; i < buttonList.Length; i++) {
+            buttonList[i].GetComponentInParent<GridButtonController>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide() { return _playerSide; }
+
+    public void EndTurn() {
+        if(buttonList[0].text == _playerSide && buttonList[1].text == _playerSide && buttonList[2].text == _playerSide) {
+            GameOver();
+        }
+    }
+
+    private void GameOver() {
+        for (int i = 0; i < buttonList.Length; i++) {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GridButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridButtonController : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+
+    private GameController _gameController;
+
+    public void SetGameControllerReference(GameController gamecontroller) {
+        this._gameController = gamecontroller;
+    }
+    
+    public void SetSpace() {
+        buttonText.text = _gameController.GetPlayerSide();
+        button.interactable = false;
+        _gameController.EndTurn();
+    }
+}
